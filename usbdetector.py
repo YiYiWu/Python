3f86c11fb1ccf29bc4cdcca17590907068851319
Yi Wu <ywu@peloton-technologies.com>
2021-03-06 22:42:44 -0800

    log msg to log.txt

--- usbdetector.py
+++ usbdetector.py
@@ -2,10 +2,11 @@ import os,string,time
 from ctypes import windll
 from datetime import datetime
 
-def get_driveStatus():
+def get_drive_status():
     devices = []
-    record_deviceBit = windll.kernel32.GetLogicalDrives()#The GetLogicalDrives function retrieves a bitmask
+    #The GetLogicalDrives function retrieves a bitmask
     #representing the currently available disk drives.
+    record_deviceBit = windll.kernel32.GetLogicalDrives()
     for label in string.ascii_uppercase : #The uppercase letters 'A-Z'
         if record_deviceBit & 1:
             devices.append(label)
@@ -13,19 +14,20 @@ def get_driveStatus():
     return devices
 
 def detect_device():
-        original = set(get_driveStatus())
+        original = set(get_drive_status())
         time.sleep(3)
-        add_device =  set(get_driveStatus())- original
-        subt_device = original - set(get_driveStatus())
-
+        add_device =  set(get_drive_status())- original
+        subt_device = original - set(get_drive_status())
+        f = open("log.txt", "a")
         if (len(add_device)):
             for drive in add_device:
-                print ("The drives added: {0} at {1}.".format(drive, datetime.now()))
+                f.write("The drives added: {0} at {1}.\n".format(drive, datetime.now()))
 
         elif(len(subt_device)):
             for drive in subt_device:
-                print ("The drives removed: {0} at {1}.".format(drive, datetime.now()))
-                    
+                f.write("The drives removed: {0} at {1}.\n".format(drive, datetime.now()))
+        f.close()
+
 if __name__ == '__main__':
     while True:
         detect_device() 
